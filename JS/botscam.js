054b3a8d13a8c73e82118d30e1dd6e8c4bb33b57
ENABLEDsam <114464225+ENABLEDsam@users.noreply.github.com>
2022-09-29 08:43:28 +0300

    JS updated, swiper update

--- JS/botscam.js
+++ JS/botscam.js
@@ -1,5 +1,14 @@
 //nodet
-var nopeNode = document.getElementById("Nah")
-var yepNode = document.getElementById("Aye")
+var nopeNode = document.getElementById("Nah");
+var yepNode = document.getElementById("Aye");
+var awnNode = document.getElementById("vas");
 
 //varit
+var one = 0;
+var two = 0;
+
+//functiot
+
+function noFunc(){
+    one ++;
+}
