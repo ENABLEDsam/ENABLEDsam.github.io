9c8e95a4c5217e84077f4127102fd411e64dd7db
ENABLEDsam <114464225+ENABLEDsam@users.noreply.github.com>
2023-04-11 08:10:46 +0300

    php added lmao

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/styyli.css">
+    <title>Vinder</title>
+</head>
+
+<body class="text-black scrollLock">
+    
+    <!--pää divit, sisältää napin ja logon-->
+    <div>
+    <h1 class="center">vinder</h1>
+    </div>
+
+    <div class="center"> 
+        <a href="swiper.html"><img class="but" src="img/PlaceholderButton.png" alt="nappula"></a>
+    </div>
+    
+    <script src="JS/botscam.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
